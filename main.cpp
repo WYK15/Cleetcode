67ea5ecaccfac314f3cace88db1c3c1d5bced1cb
wangyankun <wangyankun@ishumei.com>
2019-07-22 00:01:46 +0800

    add:647,动态规划

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
-  struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-  };
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
 class Solution {
 public:
@@ -18,9 +19,24 @@ public:
         if(p->val != q->val) return false;
         return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
+
+    int countSubstrings(string s) {
+        int len = s.length();
+        bool dp[len][len];
+        int result = 0;
+        memset(dp,0,len*len);
+        for (int i = len-1; i >=0 ; i--) {
+            for (int j = i; j < len; j++) {
+                dp[i][j] = (s[i]==s[j]) && ( (j-i)<=2 || dp[i+1][j-1] );
+                if (dp[i][j]) result++;
+            }
+        }
+        return result;
+    }
 };
 
+
 int main(){
-    cout<<"hello world"<<endl;
+    cout<<Solution().countSubstrings("aaaaa");
     return 0;
 }
